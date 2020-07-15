0017416bd61fd77c169aded59a168443c0b70e14
David Potapkin <davidosimo1@gmail.com>
2020-07-15 18:35:47 +0300

    app, restyle styles

--- src/app/pages/preview/preview.page.ts
+++ src/app/pages/preview/preview.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './preview.page.html',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  toSliderPage() {
+    this.router.navigate(['slider']);
   }
 
 }
