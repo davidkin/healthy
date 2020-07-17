605c810f505a772cbe7ff49ea63503813f59f317
David Potapkin <davidosimo1@gmail.com>
2020-07-17 14:40:17 +0300

    program-name, create program-name page

---
+++ src/app/pages/program-name/program-name.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './program-name.page.html',
+  styleUrls: ['./program-name.page.scss']
+})
+export class ProgramNamePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
