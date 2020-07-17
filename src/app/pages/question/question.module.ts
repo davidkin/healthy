5475e95a00ff31a254fbe54b7e2597417f1c9df4
David Potapkin <davidosimo1@gmail.com>
2020-07-17 14:40:47 +0300

    folder name fix

--- src/app/pages/question/question.module.ts
+++ src/app/pages/question/question.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { QuestionPageRoutingModule } from './question-routing';
+import { QuestionPageRoutingModule } from './question-routing.module';
 import { QuestionPage } from './question.page';
 import { SharedModule } from 'src/app/shared/shared.module';
 
