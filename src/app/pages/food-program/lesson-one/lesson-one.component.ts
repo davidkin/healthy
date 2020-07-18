5ceceec720235f1417beb4efb5e82da4acedfd84
David Potapkin <davidosimo1@gmail.com>
2020-07-18 16:34:03 +0300

    food-program, create layout for lesson-one

--- src/app/pages/food-program/lesson-one/lesson-one.component.ts
+++ src/app/pages/food-program/lesson-one/lesson-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lesson-one',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonOneComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  toLessonTwo() {
+    this.router.navigate(['/food-program/lesson-2']);
   }
 
 }
