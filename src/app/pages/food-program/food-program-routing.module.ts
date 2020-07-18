e0212e35ef32ec5a8f75cd3959bda50c23d2847f
David Potapkin <davidosimo1@gmail.com>
2020-07-18 15:24:10 +0300

    food-program, - add lesson-one component; - add lesson-two component; - add lesson-three component; - add lesson-four component; - add lesson-five component;

--- src/app/pages/food-program/food-program-routing.module.ts
+++ src/app/pages/food-program/food-program-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FoodProgramPage } from './food-program.page';
+import { LessonOneComponent } from './lesson-one/lesson-one.component';
+import { LessonTwoComponent } from './lesson-two/lesson-two.component';
+import { LessonThreeComponent } from './lesson-three/lesson-three.component';
+import { LessonFourComponent } from './lesson-four/lesson-four.component';
+import { LessonFiveComponent } from './lesson-five/lesson-five.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: FoodProgramPage
+    redirectTo: '/food-program/lesson-1',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: FoodProgramPage,
+
+    children: [
+      { path: 'lesson-1', component: LessonOneComponent },
+      { path: 'lesson-2', component: LessonTwoComponent },
+      { path: 'lesson-3', component: LessonThreeComponent },
+      { path: 'lesson-4', component: LessonFourComponent },
+      { path: 'lesson-5', component: LessonFiveComponent }
+    ]
   }
 ];
 
---
+++ src/app/pages/food-program/lesson-four/lesson-four.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-four',
+  templateUrl: './lesson-four.component.html',
+  styleUrls: ['./lesson-four.component.scss']
+})
+export class LessonFourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
