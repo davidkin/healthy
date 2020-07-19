39568795c42fdd4015089a960243066ec8c57e06
David Potapkin <davidosimo1@gmail.com>
2020-07-20 00:17:47 +0300

    menu, create menu and add layour, also delete extra files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor( ) { }
+  menuItems = [
+    { id: 1, title: 'Личный кабинет' },
+    { id: 2, title: 'Программы' },
+    { id: 3, title: 'Задания' },
+    { id: 4, title: 'Лидерборд' },
+    { id: 5, title: 'Новости' },
+    { id: 6, title: 'Календарь' },
+    { id: 7, title: 'Опросы' },
+    { id: 8, title: 'Магазин' },
+    { id: 9, title: 'Чат поддержки' },
+    { id: 10, title: 'Настройки профиля' },
+    { id: 11, title: 'Выход' },
+  ];
+
+  constructor(
+    private menu: MenuController
+  ) { }
 
   initializeApp() { }
 
   ngOnInit() { }
+
+  closeMenu() {
+    this.menu.close();
+  }
 }
