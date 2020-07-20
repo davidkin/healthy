2346712abcae6d30d6efd66160fc605a0aa15953
David Potapkin <davidosimo1@gmail.com>
2020-07-20 16:04:31 +0300

    individual-news, create layout

--- src/app/pages/individual-news/individual-news.page.ts
+++ src/app/pages/individual-news/individual-news.page.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IList } from 'src/app/shared/interfaces/List';
 
 @Component({
   templateUrl: './individual-news.page.html',
   styleUrls: ['./individual-news.page.scss']
 })
 export class IndividualNewsPage implements OnInit {
+
+  listData: IList[] = [
+    { img: 'assets/images/qa/apple.png', title: 'Рациональное питание -  основа здоровья и благополучия', subTitle: 'Урок 1' },
+    { img: 'assets/images/qa/apple.png', title: 'Физиология пищеварительной системы', subTitle: 'Урок 2' },
+    { img: 'assets/images/qa/apple.png', title: 'Рацион и режим питания', subTitle: 'Урок 3' },
+  ];
 
   constructor() { }
 
