4e7a306fb683123d13b3dc914077e2c376dba482
Mavinxukr <mavinx.office@gmail.com>
2020-07-20 23:52:42 +0300

    add first chat block

---
+++ src/app/pages/chat/chat.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './chat.page.html',
+  styleUrls: ['./chat.page.scss']
+})
+export class ChatPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
