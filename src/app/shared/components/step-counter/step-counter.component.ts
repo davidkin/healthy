9fa69ee9605f857fcc3e0a12eae81ffa2bfd999d
David Potapkin <davidosimo1@gmail.com>
2020-07-20 17:30:02 +0300

    individual-leaderboard, create layout

---
+++ src/app/shared/components/step-counter/step-counter.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-step-counter',
+  templateUrl: './step-counter.component.html',
+  styleUrls: ['./step-counter.component.scss']
+})
+export class StepCounterComponent implements OnInit {
+
+  @Input() time: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
