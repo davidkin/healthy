2383f948ca300e9988cc0517b9e0f68ce572dd34
David Potapkin <davidosimo1@gmail.com>
2020-07-21 05:35:05 +0300

    create-program, add create-program page

---
+++ src/app/pages/create-program/create-program.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './create-program.page.html',
+  styleUrls: ['./create-program.page.scss']
+})
+export class CreateProgramPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
